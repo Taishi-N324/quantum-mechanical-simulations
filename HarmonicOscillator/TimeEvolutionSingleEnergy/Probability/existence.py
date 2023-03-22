5a45970c825391f3d0c7d595adb208b5578177ef
Taishi Nakamura <82321333+Taishi-N324@users.noreply.github.com>
2023-03-23 01:19:25 +0900

    fix

--- HarmonicOscillator/TimeEvolutionSingleEnergy/Probability/existence.py
+++ HarmonicOscillator/TimeEvolutionSingleEnergy/Probability/existence.py
@@ -7,16 +7,6 @@ import matplotlib
 import sys
 import math
 
-matplotlib.use("Agg")
-args = sys.argv
-sim_n = int(args[1])
-
-h = 6.6260896 * 1.0E-34
-hbar = h / (2.0 * np.pi)
-me = 9.10938215 * 1.0E-31
-omega = 1.0E-15
-
-
 def hpn_calc(x, n):
     """
     Calculate the value of the Hermite polynomial of degree n at x.
@@ -40,32 +30,36 @@ def hpn(x,n):
 
 def psiOscillator(x,t,n):
     return (me * omega / hbar) ** 0.25 * hpn(cmath.sqrt(me * omega / hbar) * x, n) * cmath.exp(-1j * omega *(n+0.5) * t)
+
+
+
+
+# matplotlib.use("Agg")
+
+h = 6.6260896 * 1.0E-34
+hbar = h / (2.0 * np.pi)
+me = 9.10938215 * 1.0E-31
+# omega = 1.0E-15
+omega = 1.0
     
 wave_functions = []
 x_coordinates = []
-for j in range(140):
+for i in range(10):
     wave_function = []
     x_coordinate = []
-    for x_coor in range(-100, 100):
-        wave_function.append(psiOscillator(x_coor/1000/1.0E-7, j/10/1.0E-15, sim_n))
-        x_coordinate.append(x_coor/1000*1.0E-15)
+    for x_coor in range(-1000, 1000):
+        wave_function.append(abs(psiOscillator(x_coor/10000, 0, i))**2)
+        x_coordinate.append(x_coor/10000)
     wave_functions.append(wave_function)
     x_coordinates.append(x_coordinate)
-    
-
+        
 fig = plt.figure(figsize = (10, 6))
-def update(i, fig_title, A):
-    if i != 0:
-        plt.cla()
 
-    plt.ylim(-0.002, 0.002)
-    plt.grid()
-    plt.title("Time evolution of a single energy in a harmonic oscillator of one-dimensional quantum mechanics n={}".format(sim_n))
-    plt.scatter(x_coordinates[i], wave_functions[i], s=15, c="c")
-
-ani = FuncAnimation(fig, update, fargs = ('Initial Animation! ', 2.0), \
-    interval = 100, frames = 140)
-
-ani.save('n{}.mp4'.format(sim_n), writer="ffmpeg")
+for i in range(10):
+    plt.scatter(x_coordinates[i], wave_functions[i], s=10,label="n={}".format(i))
+plt.grid()
+plt.legend()
+plt.savefig("existence.png", format="png", dpi=300)
+plt.show()
 
 
